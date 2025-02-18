f6f29be71fa008476f487964656e8a738988113e
Shahriyor <narzullayevshaxriyor049@gmail.com>
2025-02-18 10:36:20 +0500

    Mission complated

--- controller/render.controller.js
+++ controller/render.controller.js
@@ -1,10 +1,28 @@
 let renderIndex = (req, res, next) => {
-    res.render("index", {
-      title: "Xususiy klinika",
-      join: "Rejalashtirish",
-    });
-  };
+  res.render("index", {
+    title: "Xususiy klinika",
+    join: "Rejalashtirish",
+  });
+};
+let renderAbout = (req, res, next) => {
+  res.render("about", {
+    title: "Biz haqimizda",
+  });
+};
+let renderService = (req, res, next) => {
+  res.render("service", {
+    title: "Xizmatlarimiz",
+  });
+};
+let renderContact = (req, res, next) => {
+  res.render("contact", {
+    title: "Aloqalarimiz",
+  });
+};
 
-module.exports= {
-    renderIndex
-}
+module.exports = {
+  renderIndex,
+  renderAbout,
+  renderService,
+  renderContact,
+};
--- routes/render.route.js
+++ routes/render.route.js
@@ -2,6 +2,9 @@ const express = require("express");
 let router = express.Router();
 const renderController = require("../controller/render.controller");
 router.get("/", renderController.renderIndex);
+router.get("/index", renderController.renderIndex);
+router.get("/about", renderController.renderAbout);
+router.get("/service", renderController.renderService);
+router.get("/contact", renderController.renderContact);
 
-
-module.exports = router
+module.exports = router;
